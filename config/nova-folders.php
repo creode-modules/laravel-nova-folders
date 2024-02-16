bac82ab0e14085827b25bd9867e2742d27d41e84
Liam Spedding <liam.spedding@creode.co.uk>
2024-02-16 16:34:03 +0000

    Add config variable

--- config/nova-folders.php
+++ config/nova-folders.php
@@ -25,4 +25,16 @@ return [
     */
 
     'policy' => Creode\LaravelNovaFolders\Policies\FolderPolicy::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Traffic Cop
+    |--------------------------------------------------------------------------
+    |
+    | Indicates whether Nova should check for modifications between viewing
+    | and updating a resource.
+    |
+    */
+
+    'traffic_cop' => true,
 ];
